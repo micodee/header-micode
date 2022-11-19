454e4514b7ed51bef85de77a3f1b7e579a55e733
micodee <martomhidayat>
2022-11-19 08:36:37 +0700

    header 1 done, start header 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header1 from "./header-collect/header1/Header1";
+import Header2 from "./header-collect/header2/Header2";
 
 const App = () => {
   return (
     <div className="App">
       <Header1 />
+      <Header2 />
     </div>
   );
 };
